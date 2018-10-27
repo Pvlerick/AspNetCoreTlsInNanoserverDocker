e11a984a58540c9e4d3b5a17f765b6562b19f94f
Philippe <pvlerick@gmail.com>
2018-10-27 13:48:47 +0200

    Server and Client working on Debug

---
+++ Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var delay = TimeSpan.FromSeconds(5);
+            Console.WriteLine($"Waiting {delay} for the server to start...");
+            Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+            var serverHost = Environment.GetEnvironmentVariable("SERVER_HOST");
+            var response = new HttpClient().GetStringAsync(serverHost).Result;
+            Console.WriteLine(response);
+        }
+    }
+}
